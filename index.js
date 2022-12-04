8fe720ead85497ce61b24dd701c8b8e9799115de
Ramkumar <ramkumar.srk007@gmail.com>
2022-12-04 14:00:37 +0530

    login functionality completd

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import userRoutes from './routes/user.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
